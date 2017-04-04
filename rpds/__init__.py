f4e8a26557130c337a03c7360c17219aabb9c041
Dibbo <DeflatedPickle@gmail.com>
2017-04-04 11:23:40 +0100

    Added package

---
+++ rpds/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""A multi-use Python based data storage format.
+
+A multi-use Python based data storage format that aims to maintain transparency and simplicity in file content, design
+and syntax.
+"""
+
+from .basic_interpreter import *
+
+__author__ = "DeflatedPickle/Dibbo, Pytato"
+__copyright__ = "Copyright (c) 2017 Dibbo"
+__credits__ = ["DeflatedPickle/Dibbo", "Pytato"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "DeflatedPickle/Dibbo, Pytato"
+__email__ = "DeflatedPickle@gmail.com"
+__status__ = "Prototype"
